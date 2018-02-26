329f826f9f3677103317ca1b898b6ad884a5a34c
Fred <frederic.briolet@gmail.com>
2018-02-26 16:46:22 +0100

    Created Latest component

---
+++ src/components/Latest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="latest">
+    <h1>Latest News</h1>
+  </div>
+</template>
+
+<script>
+import store from '../store/store'
+
+export default {
+  store,
+  name: 'latest',
+  mounted () {
+    console.log('latest', this)
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
