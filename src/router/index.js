9a31a3886eb9d8eb05c44d4414e5d0bf40e3da38
glegenty <g.legenty972@gmail.Com>
2018-02-28 17:08:37 +0100

    modify route to article

--- src/router/index.js
+++ src/router/index.js
@@ -29,6 +29,11 @@ export default new Router({
       path: '/search',
       name: 'Search',
       component: Search
+    },
+    {
+      path: '/article/:id',
+      name: 'Article',
+      component: Article
     }
   ]
 })
